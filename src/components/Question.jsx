5e05b5208990dc2004c971d1c7166211f886ccbe
Katiasun <katjatolok3@gmail.com>
2025-04-02 16:18:05 +0200

    Move code tag <div> with id=question from component Quiz.jsx to Question.jsx

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,3 +1,17 @@
 export default function Question() {
-  return <div id="question"></div>;
+  return (
+    <div id="question">
+      <div id="question">
+        <QuestionTimer key={activeQuestionIndex} timeout={10000} onTimeout={handleSkipAnswer} />
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <Answers
+          key={activeQuestionIndex}
+          answers={QUESTIONS[activeQuestionIndex].answers}
+          selectedAnswer={userAnswers[userAnswers.length - 1]}
+          answerState={answerState}
+          onSelect={handleSelectAnswer}
+        />
+      </div>
+    </div>
+  );
 }
