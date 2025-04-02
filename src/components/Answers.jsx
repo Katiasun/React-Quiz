0c1503545f4c233543c9adb6528a23b26743c11a
Katiasun <katjatolok3@gmail.com>
2025-04-02 10:51:28 +0200

    Move an unordered list from file Quiz.jsx to Answers.jsx

--- src/components/Answers.jsx
+++ src/components/Answers.jsx
@@ -1,1 +1,26 @@
-export default function Answers() {}
+export default function Answers({ answers, selectedAnswer, answerState }) {
+  return (
+    <ul id="answers">
+      {shuffledAnswers.current.map((answer) => {
+        const isSelected = userAnswers[userAnswers.length - 1] === answer;
+        let cssClass = "";
+
+        if (answerState === "answered" && isSelected) {
+          cssClass = "selected";
+        }
+
+        if ((answerState === "correct" || answerState === "wrong") && isSelected) {
+          cssClass = answerState;
+        }
+
+        return (
+          <li key={answer} className="answer">
+            <button className={cssClass} onClick={() => handleSelectAnswer(answer)}>
+              {answer}
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
