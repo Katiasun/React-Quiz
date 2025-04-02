352e8fd0da3065b308d9de2920ab2f0d21b74087
Katiasun <katjatolok3@gmail.com>
2025-04-02 19:58:45 +0200

    outputting Quiz Results

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,6 +1,7 @@
 import { useState, useCallback } from "react";
 import QUESTIONS from "../questions";
 import Question from "./Question";
+import Summery from "./Summary";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
@@ -17,6 +18,7 @@ export default function Quiz() {
   const handleSkipAnswer = useCallback(() => handleSelectAnswer(null));
 
   if (quizIsComplete) {
+    return <Summery userAnswers={userAnswers} />;
   }
 
   return (
